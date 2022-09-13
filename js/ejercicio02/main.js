bbc4ea1a5431aee1b04dfcb223c88b2d80bf3e48
diegosper <diego.sperber@gmail.com>
2022-09-14 01:54:50 +0200

    avances ejercicio 2 js

--- js/ejercicio02/main.js
+++ js/ejercicio02/main.js
@@ -2,11 +2,20 @@ function asignarClase(clase){
 let div  = document.getElementById('boxEstilos');
 
 div.className = clase;
-
 }
 
 function agregarClase(clase){
     let div = document.getElementById('boxEstilos');
 
     div.classList.add(clase);
+}
+
+function agregarFila (id){
+    let tabla = document.getElementById(id);
+
+    let tr = document.createElement("tr");
+    tr.className = "row";
+    tr.innerText = "NUEVA FILA"
+
+    tabla.append(tr);
 }
